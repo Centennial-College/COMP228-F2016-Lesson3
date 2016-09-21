012386f83af9c29735f7adbe8ad4ce0ac87ba378
Kevin Ma <kbma.kevin@gmail.com>
2016-09-21 09:04:24 -0400

    Initial Commit

---
+++ src/wk3_lecture/MaximumFinder.java
@@ -0,0 +1,20 @@
+package wk3_lecture;
+
+import java.util.Scanner;
+
+// Find the maximum value among 3 double values
+public class MaximumFinder {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt user for input
+		System.out.print("Enter 3 floating-point values separated by spaces: ");
+			
+		double number1 = input.nextDouble();
+		double number2 = input.nextDouble();
+		double number3 = input.nextDouble();
+	}
+
+}
